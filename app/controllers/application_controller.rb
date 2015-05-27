48e77285c35149faa9fb60abcc1b8e51c701b9e9
Robert Martin <robert.martin@nbcuni.com>
2015-05-27 12:53:12 -0700

    removed force_ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  force_ssl if: :ssl_configured?
+ # force_ssl if: :ssl_configured?
 
 
   private
